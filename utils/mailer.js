21fde1695e584339bc6b5404fb65ae80d5fcb22a
Kunj <kunjmpatel254@gmail.com>
2026-06-17 23:25:57 +0530

    some important fixes

--- utils/mailer.js
+++ utils/mailer.js
@@ -1,30 +1,49 @@
 const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
-    host: "smtp.gmail.com",
-    port: 587,
-    secure: false,
-    family: 4, // Force IPv4
+    service: "gmail",
     auth: {
         user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS
-    },
-    connectionTimeout: 60000,
-    greetingTimeout: 60000,
-    socketTimeout: 60000
+        pass: process.env.EMAIL_PASS   // app password
+    }
 });
 
 module.exports.sendAlert = async (to, subject, html) => {
-
-    try {
-        const info = await transporter.sendMail({
-            from: `"LockForge" <${process.env.EMAIL_USER}>`,
-            to,
-            subject,
-            html
-        });
-    } catch (err) {
-        console.error("EMAIL ERROR:", err);
-        throw err;
-    }
+    await transporter.sendMail({
+        from: `"Password Manager" <${process.env.EMAIL_USER}>`,
+        to,
+        subject,
+        html
+    });
 };
+
+// const nodemailer = require("nodemailer");
+
+// const transporter = nodemailer.createTransport({
+//     host: "smtp.gmail.com",
+//     port: 587,
+//     secure: false,
+//     family: 4, // Force IPv4
+//     auth: {
+//         user: process.env.EMAIL_USER,
+//         pass: process.env.EMAIL_PASS
+//     },
+//     connectionTimeout: 60000,
+//     greetingTimeout: 60000,
+//     socketTimeout: 60000
+// });
+
+// module.exports.sendAlert = async (to, subject, html) => {
+
+//     try {
+//         const info = await transporter.sendMail({
+//             from: `"LockForge" <${process.env.EMAIL_USER}>`,
+//             to,
+//             subject,
+//             html
+//         });
+//     } catch (err) {
+//         console.error("EMAIL ERROR:", err);
+//         throw err;
+//     }
+// };
