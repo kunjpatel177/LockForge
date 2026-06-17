82cffb7fd90ffd68fcbf8744c1db826696812e39
Kunj <kunjmpatel254@gmail.com>
2026-06-17 21:34:56 +0530

    some important fixes

--- utils/mailer.js
+++ utils/mailer.js
@@ -9,9 +9,9 @@ const transporter = nodemailer.createTransport({
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS
     },
-    connectionTimeout: 10000,
-    greetingTimeout: 10000,
-    socketTimeout: 10000
+    connectionTimeout: 60000,
+    greetingTimeout: 60000,
+    socketTimeout: 60000
 });
 
 module.exports.sendAlert = async (to, subject, html) => {
